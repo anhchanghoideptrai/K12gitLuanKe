048e3e74e8c93b2f238cc95b8b196ac9f3059f95
anhchanghoideptrai <luanth.bkav@gmail.com>
2021-06-04 19:48:20 +0700

    commit dau tien

---
+++ js1.js
@@ -0,0 +1,17 @@
+//var arr = [1, 9, 7, 0, 6, 7];
+
+//for (var i = 0; i < arr.length; i++) {
+    //if (arr[i] === 0) {
+        //continue;
+    //}
+    //console.log(arr[i]);
+//}
+
+console.log(1,1);
+//setTimeout(function() {
+    //console.log(10);
+    //}, 5000);
+//setInterval(function() {
+    //console.log(new Date());
+//}, 2000);    
+console.log(18,2);
---
+++ js1a.js
@@ -0,0 +1,93 @@
+//var number = 10;
+//function demo(number){
+    //number ++
+//}
+//demo(number)
+//console.log(number);
+
+function sum(a,b){
+    return a + b;
+}
+console.log(sum(2, "1-"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//function getinput() {
+    //let user = $(".user").val();
+    //let pass = $(".pass").val();
+    //console.log(user, pass);
+    //var mess = "";
+    //$(".list").html
+//}
+//function color() {
+    //let body = $("body")[0];
+    //$(body).css("background", "pink")
+//}
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let a = 1
+//let b = 2
+//let p1 = new Promise(function (res, rej) {
+    //if (a > b) {
+        //res("dung");
+    //} else {
+        //rej("sai");
+    //}
+//});
+//p1.then(function(data) {
+    //console.log(13, data);
+//}).catch(function (err) {
+    //console.log(15, err);
+//});
+
+//console.log("hello");
